0457b486901b4e4fdef4b6ba98cd47e665a22e81
Peter Lemiszki <plemiszki@gmail.com>
2018-01-28 13:40:29 -0500

    convert recurring hash into text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,23 @@
 module ApplicationHelper
 
   def convert(hash)
-    hash.to_s == "{:every=>:day}" ? "Daily" : "Custom"
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    if hash.has_key?(:every)
+      if hash[:every] == :day
+        "Daily"
+      elsif hash[:every] == :week
+        "#{hash[:on].to_s.capitalize}s"
+      elsif hash[:every] == :month
+        "Monthly"
+      elsif hash[:every] == :year
+        p hash
+        "Every #{months[hash[:month][0] - 1]}"
+      else
+        "Custom"
+      end
+    else
+      "Custom"
+    end
   end
 
 end
