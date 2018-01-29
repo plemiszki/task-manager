1c312f5eec4176fce22e5cef5099a354403d4250
Peter Lemiszki <plemiszki@gmail.com>
2018-01-29 12:00:30 -0500

    update convert method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,16 @@ module ApplicationHelper
     months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     if hash.has_key?(:every)
       if hash[:every] == :day
-        "Daily"
+        if hash.has_key?(:interval)
+          "Every #{hash[:interval]} days"
+        else
+          "Daily"
+        end
       elsif hash[:every] == :week
         "#{hash[:on].to_s.capitalize}s"
       elsif hash[:every] == :month
         "Monthly"
       elsif hash[:every] == :year
-        p hash
         "Every #{months[hash[:month][0] - 1]}"
       else
         "Custom"
