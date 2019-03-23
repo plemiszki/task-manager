96a2cdde30e8733726faa6578c17fb91db56b43a
Peter Lemiszki <plemiszki@gmail.com>
2019-03-23 11:15:01 -0400

    fix missing comma

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,7 +15,7 @@ module.exports = {
       'process.env':{
         'NODE_ENV': JSON.stringify('production')
       }
-    })
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress:{
         warnings: true
