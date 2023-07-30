d19c94f8a0d25537977eed7654b88d9b1fd8bdb4
Peter Lemiszki <plemiszki@gmail.com>
2023-07-30 17:34:00 -0400

    recipe details - use simple details

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux'
 import ReactModal from 'react-modal'
 import CurrentUser from './components/current-user.jsx'
 import TasksIndex from './components/tasks-index.jsx'
-import TasksTimeframe from './components/tasks-timeframe.jsx'
 import FutureTasksIndex from './components/future-tasks-index.jsx'
 import RecurringTasksIndex from './components/recurring-tasks-index.jsx'
 import RecurringTaskDetails from './components/recurring-task-details.jsx'
 import RecipesIndex from './components/recipes-index.jsx'
 import RecipeDetails from './components/recipe-details.jsx'
+import { SimpleDetails } from 'handy-components'
 
 import configureStore from './store/store'
 let store = configureStore();
@@ -67,7 +67,31 @@ $(document).ready(() => {
   if ($('#recipe-details')[0]) {
     ReactDOM.render(
       <Provider store={ store }>
-        <RecipeDetails />
+        <div class="container widened-container">
+          <SimpleDetails
+            entityName='recipe'
+            header='Edit Recipe'
+            hideDeleteButton={ true }
+            initialEntity={{
+              name: '',
+              category: '',
+              time: '',
+              ingredients: '',
+              prep: '',
+            }}
+            fields = {[
+              [
+                { columnWidth: 6, property: 'name' },
+                { columnWidth: 4, property: 'category' },
+                { columnWidth: 2, property: 'time' }
+              ],
+              [
+                { columnWidth: 6, property: 'ingredients', type: 'textbox', rows: 11 },
+                { columnWidth: 6, property: 'prep', columnHeader: 'Preparation', type: 'textbox', rows: 11 },
+              ],
+            ]}
+          />
+        </div>
       </Provider>,
       document.getElementById("recipe-details")
     );
