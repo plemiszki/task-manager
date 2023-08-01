f9ff7d54b6d22dc0575e833a2281a3645b0a9202
Peter Lemiszki <plemiszki@gmail.com>
2023-07-31 20:41:40 -0400

    future tasks index - remove redux

--- frontend/components/future-tasks-index.jsx
+++ frontend/components/future-tasks-index.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React, { useState, useEffect } from 'react'
+import { GrayedOut, Spinner, deleteEntity } from 'handy-components'
 import Modal from 'react-modal'
 import Moment from 'moment'
-import { Common, Index } from 'handy-components'
-import HandyTools from 'handy-tools'
 import FutureTaskNew from './future-task-new.jsx'
-import { fetchEntities, deleteEntity } from '../actions/index'
 
 const ModalStyles = {
   overlay: {
@@ -21,107 +17,82 @@ const ModalStyles = {
   }
 };
 
-class FutureTasksIndex extends React.Component {
+export default function FutureTasksIndex() {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      modalOpen: false,
-      fetching: true,
-      futureTasks: []
-    }
-  }
+  const [spinner, setSpinner] = useState(true)
+  const [modalOpen, setModalOpen] = useState(false)
+  const [futureTasks, setFutureTasks] = useState([])
 
-  componentDidMount() {
-    this.props.fetchEntities({ directory: 'future_tasks' }).then(() => {
-      this.setState({
-        fetching: false,
-        futureTasks: this.props.futureTasks
+  useEffect(() => {
+    fetch(`/api/future_tasks`)
+      .then(data => data.json())
+      .then((response) => {
+        setFutureTasks(response.futureTasks)
+        setSpinner(false)
       });
-    });
-  }
+  }, [])
 
-  clickNew() {
-    this.setState({
-      modalOpen: true
-    });
-  }
-
-  clickX(e) {
-    this.setState({
-      fetching: true
-    });
-    this.props.deleteEntity({
+  const deleteFutureTask = (id) => {
+    setSpinner(true);
+    deleteEntity({
       directory: 'future_tasks',
-      id: e.target.dataset.id
-    }).then(() => {
-      this.setState({
-        fetching: false,
-        futureTasks: this.props.futureTasks
-      });
+      id,
+    }).then((response) => {
+      const { futureTasks } = response;
+      setFutureTasks(futureTasks);
+      setModalOpen(false);
+      setSpinner(false);
     });
-  }
+  };
 
-  closeModal() {
-    this.setState({
-      modalOpen: false
-    });
-  }
-
-  render() {
-    return(
-      <div className="container widened-container index-component">
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="white-box">
-              { Common.renderSpinner(this.state.fetching) }
-              { Common.renderGrayedOut(this.state.fetching, -26, -26, 6) }
-              <h1>Future Tasks</h1>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Date</th>
-                    <th>Text</th>
-                    <th>Time Frame</th>
-                    <th>Position</th>
-                    <th>Color</th>
-                    <th className="x-button-column"></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="below-header"><td></td><td></td><td></td><td></td><td></td></tr>
-                  { this.state.futureTasks.map((task) => {
-                    return(
-                      <tr key={ task.id }>
-                        <td>{ Moment(task.date).format('l') }</td>
-                        <td>{ task.text }</td>
-                        <td>{ task.timeframe }</td>
-                        <td>{ task.addToEnd ? "End" : "Beginning" }</td>
-                        <td><div className="swatch" style={ { backgroundColor: task.color } }></div></td>
-                        <td><div className="x-button" onClick={ this.clickX.bind(this) } data-id={ task.id }></div></td>
-                      </tr>
-                    );
-                  }) }
-                </tbody>
-              </table>
-              <div className="btn btn-info" onClick={ this.clickNew.bind(this) }>Add New</div>
-            </div>
+  return (
+    <div className="handy-component container widened-container index-component">
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="white-box">
+            <h1>Future Tasks</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Text</th>
+                  <th>Time Frame</th>
+                  <th>Position</th>
+                  <th>Color</th>
+                  <th className="x-button-column"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="below-header"><td></td><td></td><td></td><td></td><td></td></tr>
+                { futureTasks.map((task) => {
+                  const { text, timeframe, addToEnd, color, id, date } = task;
+                  return(
+                    <tr key={ id }>
+                      <td>{ Moment(date).format('l') }</td>
+                      <td>{ text }</td>
+                      <td>{ timeframe }</td>
+                      <td>{ addToEnd ? "End" : "Beginning" }</td>
+                      <td><div className="swatch" style={ { backgroundColor: color } }></div></td>
+                      <td><div className="x-button" onClick={ () => deleteFutureTask(id) }></div></td>
+                    </tr>
+                  );
+                }) }
+              </tbody>
+            </table>
+            <div className="btn btn-info" onClick={ () => setModalOpen(true) }>Add New</div>
+            <GrayedOut visible={ spinner } />
+            <Spinner visible={ spinner } />
           </div>
         </div>
-        <Modal isOpen={ this.state.modalOpen } onRequestClose={ this.closeModal.bind(this) } contentLabel="Modal" style={ ModalStyles }>
-          <FutureTaskNew afterCreate={ (futureTasks) => { this.setState({ futureTasks, modalOpen: false }) } } />
-        </Modal>
       </div>
-    );
-  }
+      <Modal isOpen={ modalOpen } onRequestClose={ () => setModalOpen(false) } contentLabel="Modal" style={ ModalStyles }>
+        <FutureTaskNew
+          afterCreate={ (futureTasks) => {
+            setFutureTasks(futureTasks);
+            setModalOpen(false);
+          } }
+        />
+      </Modal>
+    </div>
+  );
 }
-
-const mapStateToProps = (reducers) => {
-  return reducers.standardReducer;
-};
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchEntities, deleteEntity }, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(FutureTasksIndex);
