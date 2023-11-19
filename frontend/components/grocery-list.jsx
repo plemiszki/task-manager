890de326e21db3b478a85e7f89e15091cd395e5a
Peter Lemiszki <plemiszki@gmail.com>
2023-11-19 16:14:35 -0500

    active list - render buttons

--- frontend/components/grocery-list.jsx
+++ frontend/components/grocery-list.jsx
@@ -29,13 +29,30 @@ export default class GroceryList extends React.Component {
     const { spinner } = this.state;
     return (
       <>
-        <div>
+        <div className="root">
           <p>Grocery list will go here.</p>
           <GrayedOut visible={ spinner } />
           <Spinner visible={ spinner } />
+          <div className="buttons">
+            <a className="btn btn-primary" rel="nofollow">Add Item</a>
+            <a className="btn btn-success" rel="nofollow">Add List</a>
+            <a className="btn btn-info recipe-button" rel="nofollow">Add From Recipe</a>
+            <a className="btn btn-warning" rel="nofollow">Clear</a>
+            <a className="btn btn-primary" rel="nofollow">Export</a>
+          </div>
         </div>
         <style jsx>{`
-
+          .root {
+            position: relative;
+            height: 100%;
+          }
+          .buttons {
+            display: flex;
+            position: absolute;
+            bottom: 0;
+            justify-content: space-between;
+            width: 100%;
+          }
         `}</style>
       </>
     );
