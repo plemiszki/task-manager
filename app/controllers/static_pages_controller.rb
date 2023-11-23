286d2600200391aa560a61b076fce4758044a5b4
Peter Lemiszki <plemiszki@gmail.com>
2023-11-23 14:53:12 -0500

    grocery list export - misc items

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,6 +15,11 @@ class StaticPagesController < ApplicationController
     grocery_stores = GroceryStore.all.includes(grocery_sections: [:grocery_items]).order(:name)
     redis = Redis.new(url: REDIS_URL, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
     item_ids = redis.smembers(REDIS_KEY).map(&:to_i)
+
+    all_item_ids_in_stores = grocery_stores.reduce([]) { |accum, store| accum += store.all_item_ids }
+    misc_item_ids = item_ids - all_item_ids_in_stores
+    @misc_items = GroceryItem.where(id: misc_item_ids)
+
     @result = []
     grocery_stores.select { |store| (store.all_item_ids & item_ids).present? }.each do |grocery_store|
       @result << {
