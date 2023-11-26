575ee3d800e807ee27bcfde960e1f99102775e5b
Peter Lemiszki <plemiszki@gmail.com>
2023-11-26 12:56:33 -0500

    grocery store details - order section items by position

--- app/models/grocery_section.rb
+++ app/models/grocery_section.rb
@@ -6,7 +6,7 @@ class GrocerySection < ActiveRecord::Base
   belongs_to :grocery_store
   alias_attribute :store, :grocery_store
 
-  has_many :grocery_section_items, dependent: :destroy
+  has_many :grocery_section_items, -> { order(:position) }, dependent: :destroy
   has_many :grocery_items, through: :grocery_section_items
   alias_attribute :items, :grocery_items
 
