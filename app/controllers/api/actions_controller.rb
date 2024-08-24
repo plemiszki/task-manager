c202fa72f524a8708b5ebcab54da5413d6f4e4a5
Peter Lemiszki <plemiszki@gmail.com>
2024-08-24 17:20:13 -0400

    add user id to redis

--- app/controllers/api/actions_controller.rb
+++ app/controllers/api/actions_controller.rb
@@ -2,9 +2,20 @@ class Api::ActionsController < ActionController::Base
 
   include Clearance::Controller
 
+  REDIS_URL = Rails.env == "development" ? "redis://localhost:6379" : ENV["REDIS_TLS_URL"]
+  REDIS_KEY = "daily-reset-early"
+
   def clear_daily_tasks
+    redis = create_redis_instance
+    redis.sadd(REDIS_KEY, current_user.id)
     Task.clear_daily_tasks!(date: DateTime.now.in_time_zone('America/New_York') + 1.day)
-    render json: { message: 'ok '}
+    render json: { message: 'ok' }
+  end
+
+  private
+
+  def create_redis_instance
+    Redis.new(url: REDIS_URL, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
   end
 
 end
