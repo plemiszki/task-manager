083aad47dcab75307346669d2d5b574ec11c7d8a
Peter Lemiszki <plemiszki@gmail.com>
2025-02-15 17:05:21 -0500

    remove user from redis

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   include Clearance::User
 
+  REDIS_URL = Rails.env == "development" ? "redis://localhost:6379" : ENV["REDIS_URL"]
+  REDIS_KEY = "daily-reset-early"
+
   has_many :tasks, dependent: :destroy
   has_many :future_tasks, dependent: :destroy
+
+  def remove_from_redis!
+    redis = Redis.new(url: REDIS_URL, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+    redis.srem(REDIS_KEY, id)
+  end
 end
