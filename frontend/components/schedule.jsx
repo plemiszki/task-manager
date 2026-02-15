858670cc403f8866ff2da3d50a9513ba2528ee8b
Peter Lemiszki <plemiszki@gmail.com>
2026-02-15 15:46:32 -0500

    indicate current day/time

--- frontend/components/schedule.jsx
+++ frontend/components/schedule.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-const DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 function buildTimeSlots() {
   const slots = [];
@@ -19,19 +27,76 @@ function formatTime(hour, minute) {
   return `${displayHour}:${minute.toString().padStart(2, "0")} ${period}`;
 }
 
+function getCurrentDayIndex() {
+  const jsDay = new Date().getDay();
+  return jsDay === 0 ? 6 : jsDay - 1;
+}
+
 const TIME_SLOTS = buildTimeSlots();
 
 export default class Schedule extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      now: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState({ now: new Date() });
+    }, 60000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   render() {
+    const { now } = this.state;
+    const currentDayIndex = getCurrentDayIndex();
+    const currentHour = now.getHours();
+    const currentMinutes = now.getMinutes();
+    const isInRange =
+      currentHour >= 6 &&
+      (currentHour < 22 || (currentHour === 22 && currentMinutes <= 30));
+
     return (
-      <div className="container" style={{ maxWidth: 1200, margin: "0 auto", padding: 20, backgroundColor: "white", borderRadius: 6, boxShadow: "1px 2px 3px 0px #e6e9ec", marginBottom: 20 }}>
+      <div
+        className="container"
+        style={{
+          maxWidth: 1200,
+          margin: "0 auto",
+          padding: 20,
+          backgroundColor: "white",
+          borderRadius: 6,
+          boxShadow: "1px 2px 3px 0px #e6e9ec",
+          marginBottom: 20,
+        }}
+      >
         <div style={{ overflowX: "auto" }}>
-          <table style={{ width: "100%", borderCollapse: "collapse", tableLayout: "fixed" }}>
+          <table
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              tableLayout: "fixed",
+            }}
+          >
             <thead>
               <tr>
                 <th style={styles.timeHeader}></th>
-                {DAYS.map((day) => (
-                  <th key={day} style={styles.dayHeader}>{day}</th>
+                {DAYS.map((day, i) => (
+                  <th
+                    key={day}
+                    style={{
+                      ...styles.dayHeader,
+                      ...(i === currentDayIndex
+                        ? { color: "#d9534f", fontWeight: "bold" }
+                        : {}),
+                    }}
+                  >
+                    {day}
+                  </th>
                 ))}
               </tr>
             </thead>
@@ -41,7 +106,7 @@ export default class Schedule extends React.Component {
                   <td style={styles.timeCell}>
                     {minute === 0 ? formatTime(hour, minute) : ""}
                   </td>
-                  {DAYS.map((day) => (
+                  {DAYS.map((day, dayIndex) => (
                     <td
                       key={`${day}-${hour}-${minute}`}
                       style={{
@@ -49,8 +114,38 @@ export default class Schedule extends React.Component {
                         borderTopWidth: minute === 0 ? 1 : 0,
                         borderTopStyle: minute === 0 ? "solid" : "none",
                         borderTopColor: "#ccc",
+                        position: "relative",
                       }}
-                    ></td>
+                    >
+                      {isInRange &&
+                        dayIndex === currentDayIndex &&
+                        hour === Math.floor(currentHour) &&
+                        minute === (currentMinutes >= 30 ? 30 : 0) && (
+                          <div
+                            style={{
+                              position: "absolute",
+                              top: ((currentMinutes % 30) / 30) * 24,
+                              left: 0,
+                              right: 0,
+                              height: 2,
+                              backgroundColor: "#d9534f",
+                              zIndex: 1,
+                            }}
+                          >
+                            <div
+                              style={{
+                                position: "absolute",
+                                left: -4,
+                                top: -3,
+                                width: 8,
+                                height: 8,
+                                borderRadius: "50%",
+                                backgroundColor: "#d9534f",
+                              }}
+                            />
+                          </div>
+                        )}
+                    </td>
                   ))}
                 </tr>
               ))}
