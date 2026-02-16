24b56bf09b52ca0ee11dc2cae289124ef2dd6796
Peter Lemiszki <plemiszki@gmail.com>
2026-02-15 20:17:56 -0500

    create block

--- app/controllers/api/schedule_blocks_controller.rb
+++ app/controllers/api/schedule_blocks_controller.rb
@@ -5,4 +5,20 @@ class Api::ScheduleBlocksController < ActionController::Base
   def index
     @schedule_blocks = ScheduleBlock.where(user_id: current_user.id).order(:weekday, :start_time)
   end
+
+  def create
+    @schedule_block = ScheduleBlock.new(schedule_block_params.merge(user_id: current_user.id))
+    if @schedule_block.save
+      @schedule_blocks = ScheduleBlock.where(user_id: current_user.id).order(:weekday, :start_time)
+      render 'index'
+    else
+      render_errors(@schedule_block)
+    end
+  end
+
+  private
+
+  def schedule_block_params
+    params.require(:schedule_block).permit(:weekday, :start_time, :end_time, :color, :text)
+  end
 end
--- frontend/components/schedule.jsx
+++ frontend/components/schedule.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { sendRequest } from "handy-components";
+import Modal from "react-modal";
+import {
+  Details,
+  sendRequest,
+  createEntity,
+  setUpNiceSelect,
+  Spinner,
+  GrayedOut,
+} from "handy-components";
 
 const DAYS = [
   "Monday",
@@ -48,6 +56,55 @@ function timeToMinutes(timeStr) {
 
 const TIME_SLOTS = buildTimeSlots();
 
+const COLORS = [
+  "rgb(234, 30, 30)",
+  "rgb(255, 175, 163)",
+  "rgb(255, 175, 36)",
+  "rgb(238, 244, 66)",
+  "rgb(92, 184, 92)",
+  "rgb(111, 138, 240)",
+  "rgb(181, 111, 240)",
+  "rgb(175, 96, 26)",
+  "rgb(210, 206, 200)",
+];
+
+const DAY_OPTIONS = DAYS.map((day, i) => ({ value: i, text: day }));
+
+function buildTimeOptions() {
+  const options = [];
+  for (let hour = 6; hour <= 22; hour++) {
+    options.push({
+      value: `${hour.toString().padStart(2, "0")}:00`,
+      text: formatTime(hour, 0),
+    });
+    if (hour < 22) {
+      options.push({
+        value: `${hour.toString().padStart(2, "0")}:30`,
+        text: formatTime(hour, 30),
+      });
+    }
+  }
+  return options;
+}
+
+const TIME_OPTIONS = buildTimeOptions();
+
+const modalStyles = {
+  overlay: {
+    background: "rgba(0, 0, 0, 0.50)",
+    zIndex: 10,
+  },
+  content: {
+    background: "#FFFFFF",
+    margin: "auto",
+    maxWidth: 600,
+    height: 429,
+    border: "solid 1px black",
+    borderRadius: "6px",
+    padding: 0,
+  },
+};
+
 export default class Schedule extends React.Component {
   constructor(props) {
     super(props);
@@ -55,6 +112,16 @@ export default class Schedule extends React.Component {
       now: new Date(),
       scheduleBlocks: [],
       scheduleHeight: 0,
+      addBlockModalOpen: false,
+      newBlock: {
+        weekday: 0,
+        startTime: "09:00",
+        endTime: "10:00",
+        color: COLORS[0],
+        text: "",
+      },
+      spinner: false,
+      errors: [],
     };
     this.scheduleRef = React.createRef();
   }
@@ -86,6 +153,66 @@ export default class Schedule extends React.Component {
     }
   }
 
+  clickAddBlock() {
+    this.setState({
+      addBlockModalOpen: true,
+      newBlock: {
+        weekday: 0,
+        startTime: "09:00",
+        endTime: "10:00",
+        color: COLORS[0],
+        text: "",
+      },
+      errors: [],
+    });
+  }
+
+  closeModal() {
+    this.setState({ addBlockModalOpen: false });
+  }
+
+  changeFieldArgs() {
+    return {
+      entity: "newBlock",
+      errorsArray: this.state.errors,
+    };
+  }
+
+  changeNewBlockField(field, value) {
+    const { newBlock } = this.state;
+    this.setState({ newBlock: { ...newBlock, [field]: value } });
+  }
+
+  submitNewBlock() {
+    const { newBlock } = this.state;
+    this.setState({ spinner: true });
+    createEntity({
+      directory: "schedule_blocks",
+      entityName: "scheduleBlock",
+      entity: {
+        weekday: newBlock.weekday,
+        startTime: newBlock.startTime,
+        endTime: newBlock.endTime,
+        color: newBlock.color,
+        text: newBlock.text,
+      },
+    }).then(
+      (response) => {
+        this.setState({
+          scheduleBlocks: response.scheduleBlocks,
+          addBlockModalOpen: false,
+          spinner: false,
+        });
+      },
+      (response) => {
+        this.setState({
+          spinner: false,
+          errors: response.errors,
+        });
+      },
+    );
+  }
+
   getBlockForCell(dayIndex, hour, minute) {
     const { scheduleBlocks } = this.state;
     const cellMinutes = hour * 60 + minute;
@@ -97,7 +224,14 @@ export default class Schedule extends React.Component {
   }
 
   render() {
-    const { now, scheduleHeight } = this.state;
+    const {
+      now,
+      scheduleHeight,
+      addBlockModalOpen,
+      newBlock,
+      spinner,
+      errors,
+    } = this.state;
     const currentDayIndex = getCurrentDayIndex();
     const currentHour = now.getHours();
     const currentMinutes = now.getMinutes();
@@ -157,10 +291,20 @@ export default class Schedule extends React.Component {
                       {minute === 0 ? formatTime(hour, minute) : ""}
                     </td>
                     {DAYS.map((day, dayIndex) => {
-                      const block = this.getBlockForCell(dayIndex, hour, minute);
-                      const startMinutes = block ? timeToMinutes(block.startTime) : 0;
-                      const endMinutes = block ? timeToMinutes(block.endTime) : 0;
-                      const durationSlots = block ? (endMinutes - startMinutes) / 30 : 0;
+                      const block = this.getBlockForCell(
+                        dayIndex,
+                        hour,
+                        minute,
+                      );
+                      const startMinutes = block
+                        ? timeToMinutes(block.startTime)
+                        : 0;
+                      const endMinutes = block
+                        ? timeToMinutes(block.endTime)
+                        : 0;
+                      const durationSlots = block
+                        ? (endMinutes - startMinutes) / 30
+                        : 0;
                       const blockHeight = durationSlots * SLOT_HEIGHT;
 
                       return (
@@ -202,7 +346,8 @@ export default class Schedule extends React.Component {
                               <div
                                 style={{
                                   position: "absolute",
-                                  top: ((currentMinutes % 30) / 30) * SLOT_HEIGHT,
+                                  top:
+                                    ((currentMinutes % 30) / 30) * SLOT_HEIGHT,
                                   left: 0,
                                   right: 0,
                                   height: 2,
@@ -244,7 +389,110 @@ export default class Schedule extends React.Component {
             boxShadow: "1px 2px 3px 0px #e6e9ec",
           }}
         >
+          <div
+            className="btn"
+            style={{ width: "100%", backgroundColor: "black", color: "white" }}
+            onClick={this.clickAddBlock.bind(this)}
+          >
+            Add Block
+          </div>
         </div>
+        <Modal
+          isOpen={addBlockModalOpen}
+          onRequestClose={this.closeModal.bind(this)}
+          contentLabel="Modal"
+          style={modalStyles}
+          onAfterOpen={() => {
+            setUpNiceSelect({
+              selector: "select",
+              func: Details.changeDropdownField.bind(this),
+            });
+          }}
+        >
+          <div className="admin-modal handy-component">
+            <div className="white-box" style={{ marginBottom: 0 }}>
+              <div className="row">
+                {Details.renderDropDown.bind(this)({
+                  columnWidth: 4,
+                  entity: "newBlock",
+                  property: "weekday",
+                  columnHeader: "Day",
+                  options: DAY_OPTIONS,
+                  optionDisplayProperty: "text",
+                  optionSortProperty: "value",
+                })}
+                {Details.renderDropDown.bind(this)({
+                  columnWidth: 4,
+                  entity: "newBlock",
+                  property: "startTime",
+                  columnHeader: "Start Time",
+                  options: TIME_OPTIONS,
+                  optionDisplayProperty: "text",
+                  optionSortProperty: "value",
+                })}
+                {Details.renderDropDown.bind(this)({
+                  columnWidth: 4,
+                  entity: "newBlock",
+                  property: "endTime",
+                  columnHeader: "End Time",
+                  options: TIME_OPTIONS,
+                  optionDisplayProperty: "text",
+                  optionSortProperty: "value",
+                })}
+              </div>
+              <div className="row">
+                {Details.renderField.bind(this)({
+                  columnWidth: 12,
+                  entity: "newBlock",
+                  property: "text",
+                })}
+              </div>
+              <div className="row">
+                <div className="col-xs-12">
+                  <h2>Color</h2>
+                  <div className="colors">
+                    {COLORS.map((color) => (
+                      <div
+                        key={color}
+                        className={
+                          "color" +
+                          (newBlock.color === color ? " selected" : "")
+                        }
+                        style={{ backgroundColor: color }}
+                        onClick={() => this.changeNewBlockField("color", color)}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </div>
+              {errors.length > 0 && (
+                <div className="row">
+                  <div className="col-xs-12">
+                    <div style={{ color: "red", fontSize: 12, marginTop: 10 }}>
+                      {errors.join(", ")}
+                    </div>
+                  </div>
+                </div>
+              )}
+              <div className="row" style={{ marginTop: 15 }}>
+                <div className="col-xs-12" style={{ textAlign: "right" }}>
+                  <div
+                    className="btn"
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                    }}
+                    onClick={this.submitNewBlock.bind(this)}
+                  >
+                    Add Block
+                  </div>
+                </div>
+              </div>
+              <GrayedOut visible={spinner} />
+              <Spinner visible={spinner} />
+            </div>
+          </div>
+        </Modal>
       </div>
     );
   }
