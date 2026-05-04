c6d2d80f8fc76e73694f6f93aa3256bc8f1409f1
Peter Lemiszki <plemiszki@gmail.com>
2026-05-03 20:42:24 -0400

    fix

--- app/models/schedule_day_variant.rb
+++ app/models/schedule_day_variant.rb
@@ -1,6 +1,6 @@
 class ScheduleDayVariant < ActiveRecord::Base
   belongs_to :user
-  has_many :schedule_blocks
+  has_many :schedule_blocks, dependent: :destroy
 
   validates :user, :name, :weekday, presence: true
   validates :name, uniqueness: { scope: [:user_id, :weekday] }
